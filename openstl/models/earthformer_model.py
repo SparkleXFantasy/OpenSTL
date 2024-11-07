97f10431558d221ea62271f4b6fdfc9013a9b7c2
songxiufeng <1559251498@qq.com>
2024-11-07 11:33:31 +0000

    fix bugs

--- openstl/models/earthformer_model.py
+++ openstl/models/earthformer_model.py
@@ -139,7 +139,7 @@ class MultiEarthFormer_Model(nn.Module):
         initial_downsample_scales = []
         input_shape = enc_dec_configs[0]['input_shape']
         for enc_dec_config in enc_dec_configs:
-            assert(enc_dec_config['input_shape'] == input_shape)
+            assert(enc_dec_config['input_shape'][:3] == input_shape[:3])
             initial_shape = enc_dec_config['initial_shape']
             initial_shapes.append(initial_shape)
             initial_downsample_scales.append([initial_shape[0] // input_shape[0], initial_shape[1] // input_shape[1], initial_shape[2] // input_shape[2]])
